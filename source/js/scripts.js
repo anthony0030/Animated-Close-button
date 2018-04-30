895af94d12069685ec990bd6d419f7fb59f2f3ca
Anthony Veaudry <anthony@veaudry.pro>
2018-04-30 15:38:31 +0300

    make the functionality of the componant better
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-    $(this).toggleClass('open');
+  $("button[class^='hamburger-menu']").click(function(){
+    $(this).toggleClass('collapsed');
   });
 });
